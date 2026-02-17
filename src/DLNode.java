8b3c85c8e203d8476d2e2bf08043feefbaf2efaa
ssarahahmed <sarahaahmed13@gmail.com>
2026-02-17 17:37:52 -0600

    Added DLList and DLNode, and completed DLNode

---
+++ src/DLNode.java
@@ -0,0 +1,13 @@
+public class DLNode {
+    Problem1Song song;
+    DLNode next;
+    DLNode previous;
+
+    public DLNode(Problem1Song song){
+        this.song = song;
+        this.next = null;
+        this.previous = null;
+    }
+
+
+}
