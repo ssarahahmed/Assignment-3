a9ce5be56620633cb2c84036d52145951b215e77
ssarahahmed <sarahaahmed13@gmail.com>
2026-02-17 09:46:47 -0600

    Added member data and Constructor for Problem 1

---
+++ src/Problem1Song.java
@@ -0,0 +1,12 @@
+public class Problem1Song {
+private String title;
+private String artist;
+private double length;
+
+    public void Song(String title, String artist, double length){
+        this.title = title;
+        this.artist = artist;
+        this.length = length;
+    }
+
+}
