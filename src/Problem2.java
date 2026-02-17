29845f5fc68d302e48791053614dad51ead43e69
ssarahahmed <sarahaahmed13@gmail.com>
2026-02-17 15:41:06 -0600

    Added empty constructor for Problem 2, and intitialized the ArrayList

---
+++ src/Problem2.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class Problem2 {
+
+private Problem1Song[] songs;
+private int size;
+private int maxSize;
+
+    public Problem2(){
+        maxSize = 10;
+        size = 0;
+        songs = new Problem1Song[maxSize];
+
+    }
+
+    public void addy()
+
+}
